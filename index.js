962a18736c0bb9fb42bb84fc234b98fd42064115
Nikita Eliseev <npeliseev@gmail.com>
2019-11-25 16:51:27 +0300

    fix

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: 'airbnb-base',
+  // env: {
+  //   es6: true,
+  //   node: true,
+  // },
+  // extends: [
+  //   'airbnb-base',
+  //   // 'plugin:jest/recommended',
+  // ],
+  // // plugins: [
+  // //   'jest',
+  // // ],
+  // parserOptions: {
+  //   ecmaVersion: 2018,
+  //   sourceType: module,
+  // },
+  // rules: {
+  //   'no-multiple-empty-lines': ['error', { max: 1 }],
+  //   'no-console': 'off',
+  //   'camelcase': 'off',
+  //   'no-await-in-loop': 1,
+  //   'import/prefer-default-export': 'off',
+  //   'class-methods-use-this': 'off',
+  // },
+  // globals: {
+  //   before: true,
+  //   beforeEach: true,
+  //   after: true,
+  //   afterEach: true,
+  //   it: true,
+  //   describe: true,
+  // },
+}
